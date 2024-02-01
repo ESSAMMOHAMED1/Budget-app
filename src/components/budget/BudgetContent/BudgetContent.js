5013927320fd446bf7d1f77501ee40226af4a159
Essam_Mohamed <99250205+ESSAMMOHAMED1@users.noreply.github.com>
2024-01-31 16:57:50 -0800

    tabs-logic

--- src/components/budget/BudgetContent/BudgetContent.js
+++ src/components/budget/BudgetContent/BudgetContent.js
@@ -1,12 +1,20 @@
 import React from 'react'
 import "./BudgetContent.css"
-import { Tabs } from '../../ui'
+import { Tabs , Tab } from '../../ui'
 const BudgetContent = () => {
   return (
     <div className="budget_content">
     <div className="container">
       <Tabs>
-        
+        <Tab title="data">
+          content1
+        </Tab>
+        <Tab title="income">
+        content2
+        </Tab>
+        <Tab title="expanses">
+        content3
+        </Tab>
       </Tabs>
     </div>
   </div>
